a2b0d3a5503638f307e5b04ad75c3f5c9e971884
Lourdes Armenta (itculiacan) <69474060+LourdesArmenta@users.noreply.github.com>
2024-02-26 16:45:15 +0000

    Ladrillos

--- miPrincipal/AppLadrillos.java
+++ miPrincipal/AppLadrillos.java
@@ -2,6 +2,23 @@ package miPrincipal;
 public class AppLadrillos {
 
     public static void menu(){
-        
+
+        System.out.println("Version Iterativa");
+        dibujar_pared_ite(10);
+        System.out.println("Version Recursiva");
+        dibujar_pared_rec(10);
     }
+    public static void dibujar_pared_ite(int n){
+        for(int i=10;i>0;i--){
+            System.out.println("XXXXXXXXXXXXXXXX");
+        }
+
+    }
+    public static void dibujar_pared_rec(int n){
+        if (n>0)
+           dibujar_pared_rec(n-1);
+        System.out.println("XXXXXXXXXXXXXXXX");  
+
+    }
+
 }
